2cf8fa9b2c73e9aa456278766fdc03ba50724635
J-WJ <Walker.Johnson.x@gmail.com>
2020-10-07 22:54:02 -0600

    New GLEE Sensor Library and AK09940 Library Updates

---
+++ Sensor Libraries/AK09940/Test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a = 5;
+    int b = 7 ;
+    if (a < b)
+    {
+        cout << "flag1" << endl;
+    }
+    if (b > a)
+    {
+        cout << "flag2" << endl;
+    }
+}
