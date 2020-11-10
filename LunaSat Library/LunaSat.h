c8cdc87b6bc1413436cb8daa9c20129b686d08eb
Andres Montealegre <66289332+anmo4276@users.noreply.github.com>
2020-11-09 21:56:31 -0700

    Update LunaSat.h

--- LunaSat Library/LunaSat.h
+++ LunaSat Library/LunaSat.h
@@ -1,37 +1,18 @@
-/*
-
-This Header file is a part of the LunaSat library. This library will provide LunaSats
-with the classes and functions necessary to initalize and communicate with both LunaSats
-and their sensors.
-
-
-Author: John Walker Johnson
-Contributors:
-Date Created: September 10th 2020
-Date Revised:September 11th 2020
-
-*/
-
-/*
-The LunaSat class is used to create each LunaSat as its own unique object.
-Within this class, there are functions used to initalize the lunaSat, query its
-sensors, and communicate with the LunaSat
-*/
-
+//First Draft LunaSat
 #include <Arduino.h>
 
 class LunaSat {
 
     public:
-        LunaSat(uint8_t idNumber,string LunaSatName);      // LunaSat Constructors
+        LunaSat();      // LunaSat Constructors
+
+        float tmp117Temp;
+        float icmAccelX, icmAccelY, icmAccelZ;
+        float ak09940MagX, ak09940MagY, ak09940MagZ;
+        unsigned long time;
         void getData();
-        void setMode();
+        void printData();
 
     private:
-        string myName;
-        uint8_t myID;
-        void initalize(void);
-
 
 };
-
