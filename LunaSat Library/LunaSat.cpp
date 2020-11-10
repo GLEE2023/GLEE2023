a8c986b82a4079b6b2536d60e585b8efbeca50fc
Andres Montealegre <66289332+anmo4276@users.noreply.github.com>
2020-11-09 21:55:18 -0700

    Update LunaSat.cpp

--- LunaSat Library/LunaSat.cpp
+++ LunaSat Library/LunaSat.cpp
@@ -1,26 +1,15 @@
-/*
+//First Draft LunaSat
+#include <Arduino.h>
 
-This cpp file is a part of the LunaSat library. This file provides the functions necessary
-for the utilization the LunaSat library and vital for the operation of the LunaSat.
-This project was created for the Great Lunar Expedition for Everyone (GLEE), a distributed
-lunar science project by the Colorado Space Grant Consortium.
+class LunaSat {
 
+    public:
+        LunaSat();      // LunaSat Constructors
 
-Author: John Walker Johnson
-Contributors:
-Date Created: September 10th 2020
-Date Revised:September 11th 2020
-
-*/
-
-#include "LunaSat.h"
-
-LunaSat::LunaSat(string lunaSatName){   //Constructor: Names LunaSat and Creates Object
-    LunaSat.myName=lunaSatName;
-
-}
-
-LunaSat::getData(){
-                                //this function will query and store sensor data
-}
-
+        float tmp117Temp;
+        float icmAccelX, icmAccelY, icmAccelZ;
+        float ak09940MagX, ak09940MagY, ak09940MagZ;
+        unsigned long time;
+        void getData();
+        void printData();
+};
