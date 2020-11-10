6a20b50182f987fe6b4954fc9a00ed458d7e5763
Andres Montealegre <66289332+anmo4276@users.noreply.github.com>
2020-11-09 21:56:02 -0700

    Update LunaSat.cpp

--- LunaSat Library/LunaSat.cpp
+++ LunaSat Library/LunaSat.cpp
@@ -1,15 +1,4 @@
 //First Draft LunaSat
-
-/*
-This cpp file is a part of the LunaSat library. This file provides the functions necessary
-for the utilization the LunaSat library and vital for the operation of the LunaSat.
-This project was created for the Great Lunar Expedition for Everyone (GLEE), a distributed
-lunar science project by the Colorado Space Grant Consortium.
-Author: John Walker Johnson
-Contributors:
-Date Created: September 10th 2020
-Date Revised:September 11th 2020
-*/
 #include <Arduino.h>
 #include <Wire.h>
 #include "AK09940.h"
