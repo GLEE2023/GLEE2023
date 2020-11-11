b48a701a39eca6ef4d35b19a7a148e168cc5ff1c
Lawson Nerenberg <lnerenbergdev@gmail.com>
2020-11-11 16:18:57 -0700

    refactor inprogress. Working on initial compile and function tests.

--- LunaSat Sketches/Activities/Magnetometer-Activity/Magnetometer-Activity.ino
+++ LunaSat Sketches/Activities/Magnetometer-Activity/Magnetometer-Activity.ino
@@ -1,33 +1,25 @@
+#include <ICM20602.h>
+#include <tmp117.h>
+
 #include <stdio.h>
-#include <LunaSat.h>
 
 // Define variable to store current time
 unsigned long currentTime;
 
 // Define lunaSat instance
 
-float readings[4];
-LunaSat lunaSat;
+TMP117 tempSensor;
+ICM20602 accler;
+int temp;
 void setup() {
-  //begin serial communication w/ LunaSat; baud=9600
   Serial.begin(9600);      
-  LunaSat lunaSat();
-  //This calls the constructor which inilializes the set of sensors and RF
+  TMP117 tempSensor();
+  ICM20602 accler();
 
-  // Wait for lunasat to acknowledge and begin communiction
-  //while (lunaSat.begin()!=true){            // While lunasat communications have not begun
-  //  delay(250);                             // Wait a bit
-  //  Serial.println("Waiting to Begin");     // Indicate waiting
-  //}
-  //Serial.println("Begin");                  // Announce begining communications
+  temp = tempSensor.getAddress();
 }
 
 void loop() {
-  currentTime=millis();
   
-  lunaSat.getData();
-  lunaSat.printData();
-
-  //TODO: Add LED change or other conditional behavior based on magnetomoter reading
   delay(500);
 }
