39e8893cfaa241813c17e220000db1d5cf26196d
Andres Montealegre <66289332+anmo4276@users.noreply.github.com>
2020-11-16 11:47:02 -0700

    Update Temperature-Activity.ino

--- LunaSat Sketches/Activities/Temperature-Activity/Temperature-Activity.ino
+++ LunaSat Sketches/Activities/Temperature-Activity/Temperature-Activity.ino
@@ -1,6 +1,6 @@
 
 #include <Arduino.h>
-#include "tmp117.h" //Might want to change this to caps for consistancy
+#include "TMP117.h" //Might want to change this to caps for consistancy
 
 TMP117 tempSensor = TMP117();
 
