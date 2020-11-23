c966cdd0aa793103db9c5f526f8aaeb275e81dfc
vonvonliu <yuli4421@colorado.edu>
2020-11-23 09:20:35 -0700

    Added some comments for each function

--- GLEE_ICM20602/ICM20602.cpp
+++ GLEE_ICM20602/ICM20602.cpp
@@ -9,6 +9,13 @@ float aRes, gRes;
 //int Ascale = AFS_2G;
 int Gscale = GFS_1000DPS;
 
+/*
+Constructor
+Parameters: sensor ID as an INT, debug mode as a BOOL
+Returns: none
+This is the constructor for this sensor that sets the
+basic variables to get started.
+*/
 ICM20602::ICM20602(int _id, bool _debug){
     ICM20602::info.name = "ICM20602 Inertial Measurement Unit";
   	ICM20602::info.address = ICM20602_slave_addr;
@@ -16,12 +23,25 @@ ICM20602::ICM20602(int _id, bool _debug){
     ICM20602::scaleA = AFS_2G;
 }
 
+/*
+Parameters: none
+Returns: whether communication with sensor was successful as a BOOL
+This function begins a transmission to the I2C slave 
+device with the given address and notifies the user
+that communication with the sensor has begun.
+*/
 bool ICM20602::begin(void){
   Wire.begin();
   Serial.println("Beginning Communication With ICM20602");
   return true;
 }
 
+/*
+Parameters: none
+Returns: none
+This function sets the clock to auto, disables the gyroscope, and sets a
+couple of the other registers.
+*/
 void ICM20602::initialize(void){
   Wire.setClock(100000);
   writeByte(ICM20602_PWR_MGMT_1, 0x01);   // Set clock set to auto
@@ -31,6 +51,13 @@ void ICM20602::initialize(void){
   writeByte(ICM20602_ACCEL_CONFIG, 0x10);   
 }
 
+/*
+Parameters: none
+Returns: The raw acceleration in LSB/G as a struct of sensor_uint16_vec_t type
+This function reads in the high and low bytes of the accerl foreach of the three 
+axis, performs a bitwise operation, then saves and returns the raw acceleration
+struct.
+*/
 sensor_uint16_vec_t ICM20602::getRawAccel(){
   uint8_t hByteX,lByteX, hByteY,lByteY, hByteZ,lByteZ;
   hByteX = readByte(ICM20602_ACCEL_XOUT_H);
@@ -45,6 +72,12 @@ sensor_uint16_vec_t ICM20602::getRawAccel(){
   return ICM20602::accelRaw;
 }
 
+/*
+Parameters: none
+Returns: The acceleration in m/s^2 as a struct of sensor_float_vec_t type
+This function converts the raw acceleration in LSB/G to 
+meters per second squared.
+*/
 sensor_float_vec_t ICM20602::getMPSAccel(){
   // TODO: Apply sensitivity factor, currently hard coded
   float MPSScale = 8.0/32768.0;
@@ -55,6 +88,12 @@ sensor_float_vec_t ICM20602::getMPSAccel(){
 }
 //testing colaberative work
 
+/*
+Parameters: current scale of the sensor as the Ascale enumeration
+Returns: the sensitivity scale factor as a FLOAT 
+This function uses a switch statement to return the sensitivity scale facor
+depending on the current sensing accuracy scale.
+*/
 float ICM20602::getSensitivity(enum Ascale scaleA){
   // TODO: Write setter for sensity a variable, possibly private
   // TODO: Set sensitivity on initialization
@@ -76,6 +115,12 @@ float ICM20602::getSensitivity(enum Ascale scaleA){
   return factor;
 }
 
+/*
+Parameters: current scale of the sensor as the Ascale enumeration
+Returns: the acceleration in G's as a sensor_float_vec_t type
+This function converts the raw acceleration in LSB/G to the acceleration in 
+G's by dividing the sensitivity factor based on the current sensitivity scale.
+*/
 sensor_float_vec_t ICM20602::getGAccel(enum Ascale scaleA){
   float factor = getSensitivity(scaleA);
   ICM20602::accelG.x = accelRaw.x/ factor;
