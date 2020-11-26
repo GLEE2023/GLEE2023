3bb5e2396fc7c06793ad64a2c3d074e8f132bd82
Lawson Nerenberg <lnerenbergdev@gmail.com>
2020-11-26 14:29:12 -0700

    Created rf + sensor compile test and updated full system test

--- LunaSat_Sketches/System_Tests/RF_with_Sensor_Compile_Test/RF_with_Sensor_Compile_Test.ino
+++ LunaSat_Sketches/System_Tests/RF_with_Sensor_Compile_Test/RF_with_Sensor_Compile_Test.ino
@@ -1,15 +1,20 @@
 #include <Arduino.h>
 #include <LunaSat.h>
+#include "GLEE_Radio.h"
 
+LunaRadio Rad;
 LunaSat lunaSat(0, true);
 lunaSat_sample_t sample;
 
 void setup() {
   lunaSat.begin(9600);
+  Rad.initialize_radio();
 }
 
 void loop() {
   sample = lunaSat.getSample();
   lunaSat.dispSample(sample);
+
+  Rad.transmit_data("sample go's here");
   delay(2000);
 }
