b5d22c68c939a171e2e418b8b67f068e3d5747f6
Lawson Nerenberg <lnerenbergdev@gmail.com>
2020-12-30 12:16:03 -0700

    Update LunaSat_Blink.ino

--- GLEE_LunaSat/Examples/LunaSat_Blink/LunaSat_Blink.ino
+++ GLEE_LunaSat/Examples/LunaSat_Blink/LunaSat_Blink.ino
@@ -3,9 +3,8 @@
 
 LunaSat lunaSat(0,false);
 
-void setup() {
-}
+void setup() {}; // Empty Setup, pinMode initialization occures upon lunaSat object construction
 
 void loop() {
-  lunaSat.blink(1,500);
-}
+  lunaSat.blink(1,500); // LunaSat.blink(int [LED #], int [on and off delay] )
+};
