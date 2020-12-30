88e40d03b0a406a2937b4eafe00639bf5263c8e3
Lawson Nerenberg <lnerenbergdev@gmail.com>
2020-12-30 12:15:53 -0700

    Added Hello World (Luna) Module Sketch

---
+++ Module_Sketches/Module_2/Hello_Luna/Hello_Luna.ino
@@ -0,0 +1,13 @@
+// GLEE 2023 Module 2: Intro to Arduino - Hello Luna
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // This sets the baud rate
+  // Serial.println("Hello Luna!");  // This prints Hello Luna
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello Luna!");  // This prints Hello Luna
+  delay(5000); // This delays the loop for 5 seconds
+}
