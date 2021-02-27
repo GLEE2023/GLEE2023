8d7395c92864f95da968990696224643e005d5c3
Lawson Nerenberg <lnerenbergdev@gmail.com>
2021-02-27 12:12:56 -0700

    Initial V5 Functionality Test Collection

---
+++ LunaSat_Sketches/System_Tests/V5_Functionality_Tests/Temperature_BF_Test/Temperature_BF_Test.ino
@@ -0,0 +1,42 @@
+// Include dependencies (libraries)
+#include <Arduino.h>
+#include "GLEE_Sensor.h"
+#include "TMP117.h"
+
+// Instantiate TMP117 class with id of one and debugging set to true
+TMP117 thermometer(1,true);
+
+// Observation configuration
+int sampleRate; // Sample rate in samples per second (Hz)
+
+// Dynamic Variable 
+float temperatureC;  // Variable to store an observed temperature in Celsius
+float temperatureF;  // Variable to store an observed temperature in Fahrenheit
+float startTime;    // Variables for timing offset calculations
+float endTime;
+
+void setup(){
+    // Begin Serial Communications (Lunasat baud rate set to 9600)
+    Serial.begin(9600);
+
+    // Set sample rate to 1 sample per second (1Hz)
+    sampleRate = 1;
+};
+
+void loop(){
+    // Record start time for letter offseting of delay to make the loop run once per second
+    startTime = millis();
+
+    // Call the getTemperatureC() function of the TMP117 thermometer  
+    temperatureC = thermometer.getTemperatureC();
+    // Call the getTemperatureF() function of the TMP117 thermometer 
+    temperatureF = thermometer.getTemperatureF();
+    // Output the temperature via seiral
+    Serial.println(temperatureC);
+    Serial.println(temperatureF);
+    // Record the end time so we know how long it took to record the measurement
+    endTime = millis();
+
+    // Delay each loop by the sample rate off set by the time it took to get the temperature
+    delay(sampleRate * 1000 - (endTime - startTime));
+};
