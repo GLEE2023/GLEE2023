7725d5dc288253e7c2befffb364fb43dc3f01e84
Lawson Nerenberg <lnerenbergdev@gmail.com>
2021-03-08 10:17:29 -0700

    added BF functionality test script to thermopile examples to serve as compile test

---
+++ GLEE_Thermopile/Examples/Thermopile_BF_Test/Thermopile_BF_Test.ino
@@ -0,0 +1,14 @@
+#include "GLEE_IR_Thermopile.h"
+
+Thermopile IRsensor;
+void setup(){
+    Serial.begin(9600);
+    IRsensor.getCalibration();
+}
+
+void loop(){
+    IRsensor.readADC();
+    Serial.print(IRsensor.getSensorTemperature());
+    Serial.println(IRsensor.getObjectTemperature());
+    delay(1000);
+}
