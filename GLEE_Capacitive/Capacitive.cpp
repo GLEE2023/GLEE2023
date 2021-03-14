605eb8b3f77893273b90346ccecbdec17a05ec17
Andres Montealegre <66289332+anmo4276@users.noreply.github.com>
2021-03-13 19:31:34 -0700

    Update Capacitive.cpp

--- GLEE_Capacitive/Capacitive.cpp
+++ GLEE_Capacitive/Capacitive.cpp
@@ -34,10 +34,28 @@ int Capacitive::getRawData(int pin){
   analogRead(pin);
 }
 
-
-// double Capacitive::getDieletricConstant(int analogReadIn){
-// In Progress
-// }
-// void Capacitive::addCalibrationPoint(int correctDielectricValue){
-// In Progress
-// }
+void Capacitive::linearRgression(vector <vector<int>> v){
+    double xAvg = 0;
+    double yAvg = 0;
+    double stdProductSum = 0;
+    double xStdSquared = 0;
+    double yStdSquared = 0;
+    double r = 0;
+    double b =0;
+    double yIntercept=0;
+    for(int i =0; i < v.size();i++){
+        xAvg = xAvg+ v[i][0];
+        yAvg = yAvg+ v[i][1];
+    }
+    xAvg= xAvg/v.size();
+    yAvg= yAvg/v.size();
+    for(int j =0; j < v.size();j++){
+        stdProductSum=stdProductSum+((v[j][0]-xAvg)*(v[j][1]-yAvg));
+        xStdSquared = xStdSquared+pow((v[j][0]-xAvg),2);
+        yStdSquared = yStdSquared+pow((v[j][1]-yAvg),2);
+    }
+    r = stdProductSum/sqrt((xStdSquared*yStdSquared));
+    b= r*(sqrt((yStdSquared/(v.size()-1))/(xStdSquared/(v.size()-1))));
+    yIntercept = yAvg - b*xAvg;
+    cout<<"y = "<<yIntercept<<" + "<< b<<"x"<<endl;
+}
