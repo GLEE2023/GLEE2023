d7397e8389f6449ef54faa86e05cf47c9e092e12
MattEMoran <mamo2629@colorado.edu>
2021-03-16 18:58:26 -0600

    Create Magnetometer_to_Compass.ino

---
+++ GLEE_AK09940/Examples/Magnetometer_to_Compass/Magnetometer_to_Compass.ino
@@ -0,0 +1,69 @@
+//Dependencies
+#include <Arduino.h>
+#include "GLEE_Sensor.h"
+#include "AK09940.h"
+#include <math.h>
+
+// Instantiate AKO9940 class with id of one and debugging set to true
+AK09940 ak09940 = AK09940(1,true);
+
+//Magnetic compass heading (calculated later)
+float heading;
+
+//Used for calculating the true north bearing
+float declination;
+
+//Used for storing the true north bearing (not currently incorporated)
+float trueHeading;
+
+void setup (){
+    // Begin Serial Communications (Lunasat baud rate set to 9600)
+    Serial.begin(9600);
+    ak09940.setDriveMode(LOW_NOISE_1); //Might change later, unsure of what drive mode is best
+    ak09940.setMeasurementMode(POWER_DOWN);
+
+    declination = 0.0; //Should be changed depending on location of LunaSat on Earth or the Moon
+};
+
+void loop (){
+    if (ak09940.getMeasurementMode() == POWER_DOWN){
+        //Set measurement mode to SINGLE_MEASURE
+        ak09940.setMeasurementMode(SINGLE_MEASURE);
+
+        //Retrieve data from magnetometer
+        ak09940.updateRawData();
+        ak09940.updateCalculatedData();
+
+        float xMag = ak09940.calculatedData.xMag;
+        float yMag = ak09940.calculatedData.yMag;
+
+        //Determine compass heading (magnetic north)
+        if(yMag > 0){
+            heading = 90 - (atan(xMag/yMag))*(180/M_PI); //M_PI = 3.141...
+        } else if (yMag < 0){
+            heading = 270 - (atan(xMag/yMag))*(180/M_PI);
+        } else {
+            if(xMag < 0){
+                heading = 180;
+            } else {
+                heading = 0.0;
+            }
+        }
+
+        //Determine if heading is indicative of magnetic north
+        if(heading==0.0){
+            Serial.println("Facing magnetic north");
+        } else {
+            Serial.print("Compass Heading (Magnetic): ");
+            Serial.println(heading);
+        }
+
+        //TODO: Determine true north heading and print to serial
+        trueHeading = heading + declination;
+
+
+    }
+    delay(1000);
+
+   
+};
