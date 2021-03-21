97813e085d4801b6a6f973c0b68a0059558d3b31
Lawson Nerenberg <lnerenbergdev@gmail.com>
2021-03-21 15:36:49 -0600

    pin A3 (PC3), not 3

--- GLEE_AK09940/AK09940.cpp
+++ GLEE_AK09940/AK09940.cpp
@@ -35,8 +35,8 @@ AK09940::AK09940(int _id, bool _debug){
 	AK09940::sensorDebug= _debug;						//debug flag status
 	
 	// Enable 1.8v-3.3v, pin pc3 set to high, level shift on initialization 
-	pinMode(3,OUTPUT);
-  	digitalWrite(3,HIGH);
+	pinMode(A3,OUTPUT);
+  	digitalWrite(A3,HIGH);
 	
 	Wire.begin(AK09940::info.address);					//begin i2c comms with sensor, must be called once
 	
