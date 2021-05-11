4072cb6734ad1a40a4ba7f44bdeb89b097e13fe2
MattEMoran <mamo2629@colorado.edu>
2021-05-11 14:45:30 -0600

    Update LunaSat_Benchmarking.ino

--- LunaSat_Sketches/System_Tests/V5_Functionality_Tests/LunaSat_Benchmarking/LunaSat_Benchmarking.ino
+++ LunaSat_Sketches/System_Tests/V5_Functionality_Tests/LunaSat_Benchmarking/LunaSat_Benchmarking.ino
@@ -9,6 +9,9 @@ LunaSat lunaSat(0, lunaSatConfig, false);
 // Custom datatypes allow for sample specialization, user can craft their own ideal sample
 lunaSat_sample_t sample;  
 
+float startTime;
+float endTime;
+
 void setup() {
     // The GLEE library architecture enables easy, interpretable and familiar programming of the lunasat
     lunaSat.begin(9600);    // Direct serial communications with computer
@@ -16,18 +19,52 @@ void setup() {
 }
 
 void loop() {
+    Serial.print("LunaSat Configuration: ");
+    Serial.print("{");
+    Serial.print(lunaSatConfig[0]);
+    Serial.print(lunaSatConfig[1]);
+    Serial.print(lunaSatConfig[2]);
+    Serial.print(lunaSatConfig[3]);
+    Serial.print(lunaSatConfig[4]);
+    Serial.print(lunaSatConfig[5]); Serial.println("}");
+    Serial.println("Getting 5 Samples...");
+    delay(1000);
     // Simple fetching of sensor observation using lunasat class 
     // Later versions will allow for dynamic sampling based on user defined config
-    float startTime = millis();
+    startTime = micros();
     sample = lunaSat.getSample(); 
     sample = lunaSat.getSample();
     sample = lunaSat.getSample();
     sample = lunaSat.getSample();
     sample = lunaSat.getSample();
-    float endTime = millis();
-
-    // Simple examples of interacting with an observation sample
-    lunaSat.dispSample(sample); // Observation samples can be directly displayed via serial
+    endTime = micros();
+    Serial.print("Time (in us) to get 5 samples: "); Serial.println(endTime-startTime);
+    Serial.print("Average time (in us) to get 1 sample: "); Serial.println((endTime-startTime)/5);
+    Serial.println("Getting 10 Samples...");
+    delay(1000);
+    startTime = micros();
+    sample = lunaSat.getSample(); 
+    sample = lunaSat.getSample();
+    sample = lunaSat.getSample();
+    sample = lunaSat.getSample();
+    sample = lunaSat.getSample();
+    sample = lunaSat.getSample(); 
+    sample = lunaSat.getSample();
+    sample = lunaSat.getSample();
+    sample = lunaSat.getSample();
+    sample = lunaSat.getSample();
+    endTime = micros();
+    Serial.print("Time (in us) to get 10 samples: "); Serial.println(endTime-startTime);
+    Serial.print("Average time (in us) to get 1 sample: "); Serial.println((endTime-startTime)/10);
+    Serial.println("Getting 100 Samples...");
+    delay(1000);
+    startTime = micros();
+    for(int i = 0; i < 100; i++){
+        sample = lunaSat.getSample();
+    }
+    endTime = micros();
+    Serial.print("Time (in us) to get 100 samples: "); Serial.println(endTime-startTime);
+    Serial.print("Average time (in us) to get 1 sample: "); Serial.println((endTime-startTime)/100);
     
-    delay(100);
+    
 }
