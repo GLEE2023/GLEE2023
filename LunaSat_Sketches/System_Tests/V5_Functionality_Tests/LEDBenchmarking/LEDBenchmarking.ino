a3e2e5b5c2f7331b8b274180b2817fe6991bcf49
MattEMoran <mamo2629@colorado.edu>
2021-05-12 12:18:38 -0600

    Created benchmarking code for LED tests

---
+++ LunaSat_Sketches/System_Tests/V5_Functionality_Tests/LEDBenchmarking/LEDBenchmarking.ino
@@ -0,0 +1,65 @@
+#include <Arduino.h>
+#include "GLEE_Sensor.h"
+
+// LED output pin configurations
+int LED1 = 4;
+int LED2 = 5;
+
+// Delay variables in milliseconds
+int on_time = 1000; // For repeated blinking
+int single_delay = 10000; // For a single blink
+
+bool singleDelay = true; // True when testing with a single blink and false when testing with repeated blinking
+
+void setup(){
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+
+    if(singleDelay){
+
+        Serial.println("Starting wait for single delay...");
+        delay(2000);
+        Serial.println("Starting in 3");
+        delay(1000);
+        Serial.println("Starting in 2");
+        delay(1000);
+        Serial.println("Starting in 1");
+        delay(1000);
+        Serial.println("Starting time now.");
+
+        delay(single_delay);
+
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, HIGH);
+        delay(1000);
+        digitalWrite(LED1, LOW);
+        digitalWrite(LED2, LOW);
+
+    } else {
+        Serial.println("Starting repeating blink...");
+        delay(2000);
+        Serial.println("Starting in 3");
+        delay(1000);
+        Serial.println("Starting in 2");
+        delay(1000);
+        Serial.println("Starting in 1");
+        delay(1000);
+        Serial.println("Starting time now.");
+    }
+};
+
+void loop(){
+    if(!singleDelay){
+
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, HIGH);
+        
+        delay(on_time);
+
+        digitalWrite(LED1, LOW);
+        digitalWrite(LED2, LOW);
+
+        delay(on_time);
+
+    }
+};
