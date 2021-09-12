e2daa9f8941503ba889c5b36ac9fa74e881ec035
MattEMoran <mamo2629@colorado.edu>
2021-09-11 20:33:32 -0600

    Added comments

--- examples/GLEE_MLX90393_Examples/Magnetometer_Basic_Example/Magnetometer_Basic_Example.ino
+++ examples/GLEE_MLX90393_Examples/Magnetometer_Basic_Example/Magnetometer_Basic_Example.ino
@@ -6,9 +6,13 @@ mlx_sample_t sample;
 
 void setup (){
     Serial.begin(9600);
+
     magnetometer.begin_I2C();
-    //magnetometer.getSensor(/*Parameter*/);
+
+    // Set gain
     magnetometer.setGain(MLX90393_GAIN_2_5X);
+
+    // Set resolution
     magnetometer.setResolution(MLX90393_X, MLX90393_RES_19);
     magnetometer.setResolution(MLX90393_Y, MLX90393_RES_19);
     magnetometer.setResolution(MLX90393_Z, MLX90393_RES_16);
@@ -19,27 +23,31 @@ void setup (){
     // Set digital filtering
     magnetometer.setFilter(MLX90393_FILTER_6);
 
-    //No apparent equivalency for setOpMode()
 };
 
 void loop (){   
-    float x, y, z;
-
-    sample = magnetometer.getSample();
+    //float x, y, z;
 
     if (magnetometer.readData(&x, &y, &z)) {
-        Serial.println("Direct Measurements");
+        sample = magnetometer.getSample();
+        /*
+        Serial.println("|Magnetic Field Axes Measurements|");
         Serial.print("X: "); Serial.print(x, 4); Serial.println(" uT");
         Serial.print("Y: "); Serial.print(y, 4); Serial.println(" uT");
         Serial.print("Z: "); Serial.print(z, 4); Serial.println(" uT");
-        Serial.print(F("Magnetic Field Magnitude: ")); 
-        Serial.println(sample.strength);
-        Serial.println("Sample Measurements");
+        */
+
+        // Print out magnetic field measurements for each axis 
+        Serial.println("|Magnetic Field Axes Measurements|");
         Serial.print("X: "); Serial.print(sample.magnetic.x, 4); Serial.println(" uT");
         Serial.print("Y: "); Serial.print(sample.magnetic.y, 4); Serial.println(" uT");
         Serial.print("Z: "); Serial.print(sample.magnetic.z, 4); Serial.println(" uT");
+
+        // Print out strength of magnetic field
+        Serial.print(F("|Magnetic Field Strength (Magnitude)|")); 
+        Serial.println(sample.strength);
     } else {
         Serial.println("Unable to read XYZ data from the sensor.");
     }
-    delay(1000); // Take samples every one sec
+    delay(1000); // Take samples every one second
 };
