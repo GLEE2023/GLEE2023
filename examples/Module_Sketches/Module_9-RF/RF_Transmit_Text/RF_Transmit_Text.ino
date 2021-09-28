0206ba66126cf5717c1e507537824911ef65dcce
Lawson Nerenberg <lnerenbergdev@gmail.com>
2021-09-28 14:09:51 -0600

    Update RF_Transmit_Text.ino

--- examples/Module_Sketches/Module_9-RF/RF_Transmit_Text/RF_Transmit_Text.ino
+++ examples/Module_Sketches/Module_9-RF/RF_Transmit_Text/RF_Transmit_Text.ino
@@ -1,13 +1,15 @@
-#include <GLEE_Radio.h>
+#include <GLEE_Radio.h> // Include Radio Library
 
+// Initialize Radio Object
 LunaRadio Rad;
 
 void setup() {
-	Serial.begin(9600);
-	Rad.initialize_radio();
+	Serial.begin(9600); // Begin serial communications at 9600 baud rate
+	Rad.initialize_radio(); // Initialize Radio (with default parameters)
 }
 
 void loop() {
-	Rad.transmit_data("Hello World!");
-	delay(1000);  
+	// Call the transmit data method and provide it the string you wish to transmit
+	Rad.transmit_data("Hello World!"); 
+	delay(1000); // Send the transmission once every ~1 sec
 }
