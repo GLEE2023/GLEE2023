44f4b68b99f3561324d59c979473baf3ef02e3ee
vonvonliu <yuli4421@colorado.edu>
2021-10-01 13:31:32 -0600

    Updated MPU library with comments and cleaned up a bit

--- src/MPU6000.cpp
+++ src/MPU6000.cpp
@@ -9,7 +9,7 @@ float aRes, gRes;
 
 /*
 Constructor
-Parameters: sensor ID as an INT, gyroscope enabled as a BOOL, debug mode as a BOOL
+Parameters: sensor ID as an INT, debug mode as a BOOL
 Returns: none
 This is the constructor for this sensor that sets the
 basic variables to get started.
@@ -48,14 +48,10 @@ bool MPU6000::begin(void){
 /*
 	Parameters: none
 	Returns: none
-	This function sets the clock to auto, disables or enables the gyroscope, and sets a
-	couple of the other registers.
+	This function sets the rate divisor and a couple of the other basic registers.
 */
 void MPU6000::initialize(void){
-    // Wire.setClock(100000);
 	reset();
-
-
 	
 	setSampleRateDivisor(0);
 
@@ -67,26 +63,45 @@ void MPU6000::initialize(void){
 	delay(100);
 	// setGyroRange(MPU6050_RANGE_500_DEG);
 
-	// setAccelerometerRange(MPU6050_RANGE_2_G); // already the default
 
   	// TODO: Dynamically initialize accel config with respect to sensitivity mode provided at initialization
 }
 
+/*
+Parameters: none
+Returns: none
+This function resets all the internal registers of the
+MPU6000 to their default values and resets signal paths.
+*/
 void MPU6000::reset(void){
 
-	writeByte(MPU6000_PWR_MGMT_1, 0b10000000);
+	//reset internal registers
+	writeByte(MPU6000_PWR_MGMT_1, 0b10000000); 
 	while(readByte(MPU6000_PWR_MGMT_1) == 0b10000000){
 		delay(1);
 	}
 	delay(100);
+	//reset gyroscope, accelerometer, and temperature sensor signal paths
 	writeByte(MPU6000_SIGNAL_PATH_RESET, 0b00000111);
 	delay(100);
 }
 
+/*
+Parameters: the rate divisor as an 8-bit unsigned integer
+Returns: none
+This function sets the sample rate divisor used to generate the
+sample rate. 
+*/
 void MPU6000::setSampleRateDivisor(uint8_t divisor){
+	//this register specifies the divider from the gyroscope output rate used to generate the Sample Rate for the MPU-6000
 	writeByte(MPU6000_SMPLRT_DIV, divisor);
 }
 
+/*
+Parameters: bandwidth wished to be set to as a mpu6000_bandwith_t enumeration
+Returns: none
+This function sets the bandwidth for the low pass filter.
+*/
 void MPU6000::setFilterBandwidth(mpu6000_bandwidth_t bandwidth){
 	writeByte(MPU6000_CONFIG, bandwidth);
 }
@@ -113,6 +128,13 @@ sensor_int16_vec_t MPU6000::getRawAcc(){
     return rawAcc;
 }
 
+/*
+	Parameters: none
+	Returns: The raw angular velocity as a struct of sensor_uint16_vec_t type
+	This function reads in the high bytes of the angular velocity for each of the three 
+	axis, performs a bitwise operation, then saves and returns the raw angular velocity
+	struct.
+*/
 sensor_int16_vec_t MPU6000::getRawGyro(){
 	sensor_int16_vec_t rawGyro;
 	uint8_t buffer[6];
@@ -132,7 +154,7 @@ sensor_int16_vec_t MPU6000::getRawGyro(){
 	Parameters: the raw acceleration as a struct of sensor_int16_vec_t type
 	Returns: the accelerations in G's as a sensor_float_vec_t type
 	This function converts the raw acceleration in LSB/G to the acceleration in 
-	G's by dividing the sensitivity factor based on the current sensitivity scale.
+	G's by dividing the sensitivity factor based on the current  accelerometer sensitivity scale.
 */
 sensor_float_vec_t MPU6000::getAcc(sensor_int16_vec_t rawAcc){
 	sensor_float_vec_t acc;
@@ -146,7 +168,7 @@ sensor_float_vec_t MPU6000::getAcc(sensor_int16_vec_t rawAcc){
 	Parameters: the raw angular velocity as a struct of sensor_int16_vec_t type
 	Returns: the angular velocity in degrees per second as a sensor_float_vec_t type 
 	This function converts the raw gyroscope reading in LSB/DPS to the angular
-	velocity in DPS by diving the sensitivity factor based on the current gyroscope
+	velocity in DPS by dividing the sensitivity factor based on the current gyroscope
 	sensitivity scale.
 */
 sensor_float_vec_t MPU6000::getGyro(sensor_int16_vec_t rawGyro){
@@ -159,7 +181,9 @@ sensor_float_vec_t MPU6000::getGyro(sensor_int16_vec_t rawGyro){
 
 /*
 	Parameters: none
-	Returns: 3 axis acceleration in G's
+	Returns: 3-axis acceleration in G's
+	This function converts the raw acceleration to accleration
+	in G's using the getAcc function and returns it.
 */
 sensor_float_vec_t MPU6000::getSample(){
 	sensor_int16_vec_t rawAcc = getRawAcc();
@@ -169,7 +193,9 @@ sensor_float_vec_t MPU6000::getSample(){
 
 /*
 	Parameters: none
-	Returns: 3 axis acceleration in G's
+	Returns: 3-axis angular velocity in degrees per second
+	This function converts the raw angular velocity  to DPS
+	using the getGyro function.
 */
 sensor_float_vec_t MPU6000::getGyroSample(){
 	sensor_int16_vec_t rawGyro = getRawGyro();
@@ -177,12 +203,38 @@ sensor_float_vec_t MPU6000::getGyroSample(){
 	return(gyro);
 }
 
+/*
+Parameters: the raw acceleration as a struct of sensor_int16_vec_t type
+Returns: the accelerations in G's as a sensor_float_vec_t type
+This function converts the raw acceleration in LSB/G to the acceleration in 
+G's by dividing the sensitivity factor based on the current sensitivity scale.
+*/
+sensor_float_vec_t MPU6000::getGAccel(sensor_int16_vec_t rawAccel){
+	sensor_float_vec_t accelG;
+	accelG.x = (float) rawAccel.x / MPU6000::accel_scale;
+    accelG.y = (float) rawAccel.y / MPU6000::accel_scale;
+    accelG.z = (float) rawAccel.z / MPU6000::accel_scale;
+	return accelG;
+}
 
 /*
-Parameters: new scale of the sensor wished to be set to as an Ascale enumeration
+Parameters: the accleration in G's as a struct of sensor_float_vec_t type
+Returns: The acceleration in m/s^2 as a struct of sensor_float_vec_t type
+This function converts the acceleration in G's to meters per second squared.
+*/
+sensor_float_vec_t MPU6000::getMPSAccel(sensor_float_vec_t GAccel){
+	sensor_float_vec_t accelMPS;
+	accelMPS.x = GAccel.x * MPU_ONE_G;
+	accelMPS.y = GAccel.y * MPU_ONE_G;
+	accelMPS.z = GAccel.z * MPU_ONE_G;
+	return accelMPS;    
+}
+
+/*
+Parameters: new scale of the sensor wished to be set to as an mpu6000_accel_range_t enumeration
 Returns: none
 This function allows a new scale to be passed in, with the global variable 
-current scale set to the new scale, and writing the accelration configuration
+accel_scale set to the new scale, and writesthe accelration configuration
 based on the new scale.
 */
 void MPU6000::setAccelRange(mpu6000_accel_range_t new_range){
@@ -212,7 +264,7 @@ void MPU6000::setAccelRange(mpu6000_accel_range_t new_range){
 Parameters: new scale of the sensor wished to be set to as an Gscale enumeration
 Returns: none
 This function allows a new scale to be passed in with the global variable 
-current gyroscope scale set to the new scale and writing the gyroscope configuration
+gyro_scale set to the new scale and writing the gyroscope configuration
 based on the new scale.
 */
 void MPU6000::setGyroRange(mpu6000_gyro_range_t new_range){
@@ -239,46 +291,9 @@ void MPU6000::setGyroRange(mpu6000_gyro_range_t new_range){
 	}
 }
 
-
-/*
-Parameters: the raw acceleration as a struct of sensor_int16_vec_t type
-Returns: the accelerations in G's as a sensor_float_vec_t type
-This function converts the raw acceleration in LSB/G to the acceleration in 
-G's by dividing the sensitivity factor based on the current sensitivity scale.
-*/
-sensor_float_vec_t MPU6000::getGAccel(sensor_int16_vec_t rawAccel){
-	sensor_float_vec_t accelG;
-	//accelG.x = (float) rawAccel.x / ICM20602::currentAccelFactor;
-    //accelG.y = (float) rawAccel.y / ICM20602::currentAccelFactor;
-    //accelG.z = (float) rawAccel.z / ICM20602::currentAccelFactor;
-	accelG.x = (float) rawAccel.x / MPU6000::accel_scale;
-    accelG.y = (float) rawAccel.y / MPU6000::accel_scale;
-    accelG.z = (float) rawAccel.z / MPU6000::accel_scale;
-	return accelG;
-}
-
-/*
-Parameters: the accleration in G's as a struct of sensor_float_vec_t type
-Returns: The acceleration in m/s^2 as a struct of sensor_float_vec_t type
-This function converts the raw accelerations in LSB/G to 
-meters per second squared.
-*/
-sensor_float_vec_t MPU6000::getMPSAccel(sensor_float_vec_t GAccel){
-	sensor_float_vec_t accelMPS;
-	accelMPS.x = GAccel.x * MPU_ONE_G;
-	accelMPS.y = GAccel.y * MPU_ONE_G;
-	accelMPS.z = GAccel.z * MPU_ONE_G;
-	return accelMPS;    
-}
-
-
-
-
-
-
 /*
 Parameters: none
-Returns: the accelerometer sensitivity scale factor as a FLOAT 
+Returns: the accelerometer sensitivity scale factor as a float 
 This function uses a switch statement to return the sensitivity scale factor
 for the acceleration depending on the current sensing accuracy scale.
 */
@@ -302,102 +317,3 @@ float MPU6000::getAccelSensitivity(){
 	accel_scale = factor;
 	return factor;  
 }
-
-
-/* Begin Funtion update TODO */
-
-/*
-Parameters: the current raw acceleration to be saved as a struct of sensor_int16_vec_t type
-Returns: none
-This function saves the the raw acceleration given by the argument into
-the private type sensor_int16_vec_t accelRaw struct. 
-*/
-// void MPU6000::updateRawAccel(sensor_int16_vec_t rawAccel){
-// 	MPU6000::accelRaw = rawAccel;
-// }
-
-/*
-Parameters: the accleration in G's as a struct of sensor_float_vec_t type
-Returns: The acceleration in m/s^2 as a struct of sensor_float_vec_t type
-This function converts the raw accelerations in LSB/G to 
-meters per second squared.
-*/
-// sensor_float_vec_t MPU6000::getMPSAccel(sensor_float_vec_t GAccel){
-// 	sensor_float_vec_t accelMPS;
-// 	accelMPS.x = GAccel.x * IMU_ONE_G;
-// 	accelMPS.y = GAccel.y * IMU_ONE_G;
-// 	accelMPS.z = GAccel.z * IMU_ONE_G;
-// 	return accelMPS;    
-// }
-
-/*
-Parameters: the current accleration in meters per second to be saved as a struct of sensor_float_vec_t type 
-Returns: none
-This function saves the acceleration in meters per second given by the argument into
-the private type sensor_float_vec_t accelMPS struct.
-*/
-// void MPU6000::updateMPSAccel(sensor_float_vec_t MPSAccel){
-// 	MPU6000::accelMPS = MPSAccel;
-// }
-
-/*
-Parameters: current scale of the sensor as the Ascale enumeration
-Returns: the accelerations in 
-debug mode set to true, if true data interfacing
-classes and everything working and responds to output for datalog
-*/
-// sensor_float_vec_t MPU6000::getGAccel_fuzzed(){
-//     MPU6000::accelG.x = currentAccelFactor*1;
-//     MPU6000::accelG.y = currentAccelFactor*2;
-//     MPU6000::accelG.z = currentAccelFactor*3;
-// 	return MPU6000::accelG;
-// }
-
-/*
-Parameters: the current accleration in G's to be saved as a struct of sensor_float_vec_t type
-Returns: none
-This function saves the acceleration in G's given by the argument into 
-the private type sensor_float_vec_t accelG struct
-*/
-// void MPU6000::updateGAccel(sensor_float_vec_t GAccel){
-// 	MPU6000::accelG = GAccel;
-// }
-
-/*
-Parameters: none
-Returns: The raw angular velocity in LSB/DPS as a struct of sensor_uint16_vec_t type
-This function reads in the high bytes of the angular velocity for each of the three 
-axis, performs a bitwise operation, then saves and returns the raw angular velocity
-struct.
-*/
-// sensor_int16_vec_t MPU6000::getRawAngVel(){
-// 	sensor_int16_vec_t RawAngVel;
-// 	RawAngVel.x = read2Byte(MPU6000_GYRO_XOUT_H);
-// 	RawAngVel.y = read2Byte(MPU6000_GYRO_YOUT_H);
-// 	RawAngVel.z = read2Byte(MPU6000_GYRO_ZOUT_H);
-// 	return RawAngVel;
-// }
-
-
-
-/*
-Parameters: the current raw angular velocity to be saved as a struct of sensor_int16_vec_t type
-Returns: none
-This function saves the raw angular velocity given by the argument into 
-the private type sensor_int16_vec_t angVelRaw struct.
-*/
-// void MPU6000::updateRawAngVel(sensor_int16_vec_t rawAngVel){
-// 	MPU6000::angVelRaw = rawAngVel;
-// }
-
-
-
-/*
-Parameters: the current angular velocity in degrees per second to be saved as a struct of sensor_float_vec_t type
-Returns: none
-This function saves the angular velocity in degrees per second given by the argument into
-the private type sensor_float_vec_t angVelDPS struct.
-*/
-// void MPU6000::updateDPSAngVel(sensor_float_vec_t DPSAngVel){
-// 	MPU6000::angVelDPS = DPSAngVel;
-// }
