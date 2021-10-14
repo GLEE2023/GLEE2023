d4867c7502ed5b1fb489e4e51e029d79497fa5fa
Lawson Nerenberg <lnerenbergdev@gmail.com>
2021-10-14 11:28:10 -0600

    Changed from cardinal directions to "Up, left, down right"

--- examples/Module_Sketches/Module_6-Magnetometer-Sensor/Magnetometer_Vector_Plot/Magnetometer_Vector_Plot.ino
+++ examples/Module_Sketches/Module_6-Magnetometer-Sensor/Magnetometer_Vector_Plot/Magnetometer_Vector_Plot.ino
@@ -92,21 +92,21 @@ void loop (){
   String direction_str = "";
   // Determine the direction based on the angle
   if (angle <= 22.5 || angle > 337.5){
-      direction_str = "North";
+      direction_str = "Up";
   } else if (angle <= 67.5 && angle > 22.5){
-      direction_str = "Northwest";
+      direction_str = "Up Left";
   } else if (angle <= 112.5 && angle > 67.5){   
-      direction_str = "West";
+      direction_str = "Left";
   } else if (angle <= 157.5 && angle > 112.5){  
-      direction_str = "Southwest";
+      direction_str = "Down Left";
   } else if (angle <= 202.5 && angle > 157.5){    
-      direction_str = "South";
+      direction_str = "Down";
   } else if (angle <= 247.5 && angle > 202.5){   
-      direction_str = "Southeast";
+      direction_str = "Down Right";
   } else if (angle <= 292.5 && angle > 247.5){    
-      direction_str = "East";
+      direction_str = "Right";
   } else if (angle <= 337.5 && angle > 292.5){   
-      direction_str = "Northeast";
+      direction_str = "Up Right";
   }
 
   // Calculates magnitude of angle from x and y axes measurements only
