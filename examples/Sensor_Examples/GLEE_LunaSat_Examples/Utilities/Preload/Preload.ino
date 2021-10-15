19b5e7ef3d41181c588b993510d315d61c23bdee
Lawson Nerenberg <lnerenbergdev@gmail.com>
2021-10-15 15:35:19 -0600

    added preload sketch

---
+++ examples/Sensor_Examples/GLEE_LunaSat_Examples/Utilities/Preload/Preload.ino
@@ -0,0 +1,19 @@
+// LunaSat ship code
+int LED1 = 4;
+void setup() {
+  pinMode(LED1, OUTPUT);
+  digitalWrite(LED1, HIGH);
+  delay(1000);
+  digitalWrite(LED1, LOW);
+  delay(1000);
+  digitalWrite(LED1,HIGH);
+  delay(1000);
+  digitalWrite(LED1, LOW);
+  delay(1000);
+  digitalWrite(LED1,HIGH);
+  delay(1000);
+  digitalWrite(LED1, LOW);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+}
