670ea29cc29d1777cf2fa2fee341420e86a8b99b
MattEMoran <mamo2629@colorado.edu>
2021-10-29 14:45:55 -0600

    Fixed issue where samples were not being retrieved

--- examples/LunaSat_Sketches/System_Tests/V5_Functionality_Tests/SensorSampleBenchmarking/SensorSampleBenchmarking.ino
+++ examples/LunaSat_Sketches/System_Tests/V5_Functionality_Tests/SensorSampleBenchmarking/SensorSampleBenchmarking.ino
@@ -1,7 +1,7 @@
 #include <LunaSat.h>    
 
 // Set lunasat configuration (1's equates to default mode)
-int lunaSatConfig[6] = {0,0,0,0,0,0}; // Configuration format: {TMP117, ICM20602, AK09940, TIPS1385, CAP, SX1272}
+int lunaSatConfig[6] = {1,1,1,1,1,1}; // Configuration format: {TMP117, ICM20602, AK09940, TIPS1385, CAP, SX1272}
 
 // LunaSat object initialization is used for declaration of parameters such as ID and debugging mode
 LunaSat lunaSat(0, lunaSatConfig, false);
@@ -25,15 +25,13 @@ void performTest(){
     Serial.println();
     Serial.print("LunaSat Configuration: ");    // Print current LunaSat Configuration
     Serial.print("{");
-    Serial.print(lunaSatConfig[0]); Serial.print(",");
-    Serial.print(lunaSatConfig[1]); Serial.print(",");
-    Serial.print(lunaSatConfig[2]); Serial.print(",");
-    Serial.print(lunaSatConfig[3]); Serial.print(",");
-    Serial.print(lunaSatConfig[4]); Serial.print(",");
-    Serial.print(lunaSatConfig[5]); Serial.println("}");
+    Serial.print(String(lunaSatConfig[0])+",");
+    Serial.print(String(lunaSatConfig[1])+",");
+    Serial.print(String(lunaSatConfig[2])+",");
+    Serial.print(String(lunaSatConfig[3])+",");
+    Serial.print(String(lunaSatConfig[4])+",");
+    Serial.print(String(lunaSatConfig[5])+"}");
     Serial.println();
-
-    //Serial.println("LunaSat Configuration: {" + to_string(lunaSatConfig[0]) + "," + to_string(lunaSatConfig[1]) + "," + to_string(lunaSatConfig[2]) + "," + to_string(lunaSatConfig[3]) + "," + to_string(lunaSatConfig[4]) + "," + to_string(lunaSatConfig[5]) + "}");
 
     delay(100);
     
@@ -47,7 +45,6 @@ void performTest(){
 
     Serial.print("Time (in microseconds) to get 1 sample: "); Serial.println(endTime-startTime);
     
-    
     //Retrieve 5 samples
     Serial.println("Getting 5 Samples...");
     delay(100);
@@ -60,8 +57,7 @@ void performTest(){
 
     Serial.print("Time (in microseconds) to get 5 samples: "); Serial.println(endTime-startTime);
     Serial.print("Average time (in microseconds) to get 1 sample: "); Serial.println((endTime-startTime)/5);
-    
-    
+     
     //Retrieve 10 samples
     Serial.println("Getting 10 Samples...");
     delay(100);
@@ -85,22 +81,21 @@ void performTest(){
     }
     endTime = micros();
 
-    //Serial.print("Time (in microseconds) to get 100 samples: "); Serial.println(endTime-startTime);
     Serial.print("Average time (in microseconds) to get 1 sample: "); Serial.println((endTime-startTime)/100);
     delay(1000);
 }
 
 void setup() {
-    lunaSat.begin(9600);    // Direct serial communications with computer
-    delay(5000);
+    Serial.begin(9600); // Direct serial communications with computer
 
+    lunaSat.begin(9600);    
+    delay(5000);
+    
     if(!performAllTests){   // For a single configuration
         performTest();
     } else {                // For all configurations
-        //Serial.println("Performing all possible tests.");
-        //Serial.println();
         //https://www.geeksforgeeks.org/program-decimal-binary-conversion/
-
+        lunaSatConfig[0] = lunaSatConfig[1] = lunaSatConfig[2] = lunaSatConfig[3] = lunaSatConfig[4] = lunaSatConfig[5] = 0;
         for(int n = 0; n < 64; n++){
             int j = 0;
             int k = n;
@@ -108,6 +103,9 @@ void setup() {
                 lunaSatConfig[j] = k % 2;
                 k = k / 2;
                 j++;
+            }
+            for(int i = 0; i < 6; i++){
+                lunaSat.info.conf[i] = lunaSatConfig[i];
             }
             performTest();
 
