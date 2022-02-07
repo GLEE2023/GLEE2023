7dfbf71ee0d79a5745d5e9f390fa6a3bec5ef75f
vonvonliu <yuli4421@colorado.edu>
2022-02-07 15:55:41 -0700

    Update MemoryBenchmarking.ino

--- examples/LunaSat_Sketches/System_Tests/V5_Functionality_Tests/MemoryBenchmarking/MemoryBenchmarking.ino
+++ examples/LunaSat_Sketches/System_Tests/V5_Functionality_Tests/MemoryBenchmarking/MemoryBenchmarking.ino
@@ -2,48 +2,33 @@
 #include "GLEE_Sensor.h"
 #include "TMP117.h"
 
-
-<<<<<<< Updated upstream
-int sizeOfBuffer = 12;
-char buff[12] = {'\0'};
-=======
-int sizeOfBuffer = 22;
-char buff[22] = {'\0'};
->>>>>>> Stashed changes
+int sizeOfBuffer = 20;
+char buff[20] = {'\0'};
 
 char tempBuffer[5] = {' '};
 
 double temperatureC;
 String bufString;
 
-<<<<<<< Updated upstream
-int numOfSamples = 1;
-=======
 int numOfSamples = 4;
->>>>>>> Stashed changes
 
 float startTime;    // Variables for timing offset calculations
 float endTime;
 
-<<<<<<< Updated upstream
-int timeBetweenSample = 1000; // Time between samples in ms 
-=======
+int timeBetweenSample = 3000; // Time between samples in ms 
 int counter = 0;
 
-int timeBetweenSample = 3000; // Time between samples in ms 
->>>>>>> Stashed changes
 
 // LunaSat object initialization is used for declaration of parameters such as ID and debugging mode
 TMP117 thermometer(1,false);
 
 void setup(){
     Serial.begin(9600);
-<<<<<<< Updated upstream
-    memset(buff, 0, 12);
-=======
+
+
     memset(buff, 0, 12); // sets the first 12 bytes of buff to 0
 
->>>>>>> Stashed changes
+
     /*
     temperatureC = thermometer.getTemperatureC();
     snprintf(tempBuffer,5,"%d",int(temperatureC));
@@ -55,28 +40,6 @@ void setup(){
     }
     */
   
-<<<<<<< Updated upstream
-    int counter = 0;
-    while(counter < numOfSamples){
-        counter++;
-        startTime = millis();
-        temperatureC = thermometer.getTemperatureC();
-        dtostrf(temperatureC, 4, 2, tempBuffer);
-        strcat(buff, tempBuffer);
-        delay(10);
-        strcat(buff,",");
-        delay(10);
-        bufString = "";
-        for(int i = 0; i < sizeOfBuffer; i++){
-            bufString = bufString + buff[i];
-        }
-        Serial.println("Buffer: " + bufString);
-        tempBuffer[5] = {' '};
-        endTime = millis();
-        delay(timeBetweenSample - (endTime - startTime));
-    }
-=======
->>>>>>> Stashed changes
     /*
     Serial.println("Checking buffer...");
     bufString = "";
@@ -90,8 +53,6 @@ void setup(){
 }
 
 void loop(){
-<<<<<<< Updated upstream
-=======
   //if(counter < numOfSamples){
     //Serial.println(counter);
     startTime = millis();
@@ -112,7 +73,6 @@ void loop(){
     delay(timeBetweenSample - (endTime - startTime));
     //counter++;
   //}
->>>>>>> Stashed changes
     /*
     startTime = millis();
     temperatureC = thermometer.getTemperatureC();
@@ -121,8 +81,5 @@ void loop(){
     endTime = millis();
     delay(timeBetweenSample - (endTime - startTime));
     */
-<<<<<<< Updated upstream
+
 }
-=======
-}
->>>>>>> Stashed changes
