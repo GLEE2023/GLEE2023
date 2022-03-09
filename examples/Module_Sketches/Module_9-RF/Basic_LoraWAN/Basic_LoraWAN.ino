dbf103780efaa2958131a5b86a8d76b727b4f029
Lawson Nerenberg <lnerenbergdev@gmail.com>
2022-03-09 09:56:11 -0700

    Created basic lorawan sketch
    Should work with current config (even without DI02 and DI05). Add keys once gateway is updated

---
+++ examples/Module_Sketches/Module_9-RF/Basic_LoraWAN/Basic_LoraWAN.ino
@@ -0,0 +1,74 @@
+/**
+ * Based on Beelan LoraWAN Class A example - Ivan Moreno, Eduardo Contreras June 2019
+ */
+#include <lorawan.h>
+
+//ABP Credentials 
+const char *devAddr = "00000000";
+const char *nwkSKey = "00000000000000000000000000000000";
+const char *appSKey = "00000000000000000000000000000000";
+
+const unsigned long interval = 10000;    // 10 s interval to send message
+unsigned long previousMillis = 0;  // will store last time message sent
+unsigned int counter = 0;     // message counter
+
+char myStr[50];
+char outStr[255];
+byte recvStatus = 0;
+
+const sRFM_pins RFM_pins = {
+  .CS = 10,
+  .RST = 9,
+  .DIO0 = 2,
+  .DIO1 = 3,
+  .DIO2 = -1,
+  .DIO5 = -1,
+};
+
+void setup() {
+  // Setup loraid access
+  Serial.begin(9600);
+  while(!Serial);
+  if(!lora.init()){
+    Serial.println("SX1272 Initialization failed");
+    delay(5000);
+    return;
+  }
+
+  // Set LoRaWAN Class change CLASS_A or CLASS_C
+  lora.setDeviceClass(CLASS_A);
+
+  // Set Data Rate
+  lora.setDataRate(SF8BW125);
+
+  // set channel to random
+  lora.setChannel(MULTI);
+  
+  // Put ABP Key and DevAddress here
+  lora.setNwkSKey(nwkSKey);
+  lora.setAppSKey(appSKey);
+  lora.setDevAddr(devAddr);
+}
+
+void loop() {
+  // Check interval overflow
+  if(millis() - previousMillis > interval) {
+    previousMillis = millis(); 
+
+    sprintf(myStr, "Counter-%d", counter); 
+
+    Serial.print("Sending: ");
+    Serial.println(myStr);
+    
+    lora.sendUplink(myStr, strlen(myStr), 0, 1);
+    counter++;
+  }
+
+  recvStatus = lora.readData(outStr);
+  if(recvStatus) {
+    Serial.println(outStr);
+  }
+  
+  // Check Lora RX
+  lora.update();
+}
