a38949a05c5cdb497f482aa55796b7610c6bb918
vonvonliu <yuli4421@colorado.edu>
2022-03-29 15:23:21 -0600

    Added Beelan

---
+++ extras/Beelan-LoRaWAN/src/arduino-rfm/AES-128.h
@@ -0,0 +1,41 @@
+/******************************************************************************************
+* Copyright 2017 Ideetron B.V.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************************/
+/****************************************************************************************
+* File:     AES-128.h
+* Author:   Gerben den Hartog
+* Compagny: Ideetron B.V.
+* Website:  http://www.ideetron.nl/LoRa
+* E-mail:   info@ideetron.nl
+****************************************************************************************/
+/****************************************************************************************
+* Created on:         13-01-2017
+* Supported Hardware: ID150119-02 Nexus board with RFM95
+****************************************************************************************/
+
+#ifndef AES128_H
+#define AES128_H
+
+/*
+********************************************************************************************
+* FUNCTION
+********************************************************************************************
+*/
+
+void AES_Encrypt(unsigned char *Data, unsigned char *Key);
+
+#endif
+
---
+++ extras/Beelan-LoRaWAN/src/arduino-rfm/lorawan-arduino-rfm.h
@@ -0,0 +1,138 @@
+/*
+    lorawan-arduino-rfm.h
+    Copyright © 2018 lora.id. All right reserved.
+
+    Author: Andri Rahmadhani
+    Date: 2018-04-25
+
+    Encapsulate Ideetron LoRaWAN simple node demonstrator
+    *This fimrware supports
+        *Over The Air Activation
+        *Activation By Personalization
+        *Class switching between Class A and Class C motes
+        *Channel hopping
+        *
+        *The following settings kan be done
+        *Channel Receive and Transmit
+        *Datarate Receive and Transmit
+        *Tranmit power
+        *Confirmed or unconfirmed messages
+        *Device Address
+        *Application Session Key
+        *Network Session Key
+        *Device EUI
+        *Application EUI
+        *Application key
+        *Mote Class
+
+    Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+*/
+
+#ifndef _LORAWAN_ARDUINO_RFM_H_
+#define _LORAWAN_ARDUINO_RFM_H_
+
+#include <SPI.h>
+#include <Arduino.h>
+#include "AES-128.h"
+#include "Encrypt.h"
+#include "RFM95.h"
+#include "LoRaMAC.h"
+#include "Struct.h"
+#include "Config.h"
+
+/*
+********************************************************************************************
+* TYPE DEFINITION
+********************************************************************************************
+*/
+
+#define LORAWAN_VERSION "1.0.0"
+/*
+*****************************************************************************************
+* CLASS
+*****************************************************************************************
+*/
+
+class LoRaWANClass
+{
+public:
+    LoRaWANClass();
+    ~LoRaWANClass();
+
+    bool init(void);
+    bool join(void);
+    void sleep(void);
+    void wakeUp(void);
+    void setDeviceClass(devclass_t dev_class);
+    // OTAA credentials
+    void setDevEUI(const char *devEUI_in);
+    void setAppEUI(const char *appEUI_in);
+    void setAppKey(const char *appKey_in);
+    // ABP credentials
+    void setNwkSKey(const char *NwkKey_in);
+    void setAppSKey(const char *ApskKey_in);
+    void setDevAddr(const char *devAddr_in);
+    void sendUplink(char *data, unsigned int len, unsigned char confirm, unsigned char mport);
+    void setDataRate(unsigned char data_rate);
+    void setChannel(unsigned char channel);
+    unsigned char getChannel();
+    unsigned char getDataRate();
+    void setTxPower1(unsigned char power_idx);
+    void setTxPower(int level, txPin_t pinTx);
+    int getRssi();
+    int readData(char *outBuff);
+    bool readAck(void);
+    void update(void);
+
+    // frame counter
+    unsigned int getFrameCounter();
+    void setFrameCounter(unsigned int FrameCounter);
+
+private:
+    void randomChannel();
+
+private:
+    // Messages
+    unsigned char Data_Tx[MAX_UPLINK_PAYLOAD_SIZE];
+    sBuffer Buffer_Tx;
+    unsigned char Data_Rx[MAX_DOWNLINK_PAYLOAD_SIZE];
+    sBuffer Buffer_Rx;
+    sLoRa_Message Message_Rx;
+
+    // Declare ABP session
+    unsigned char Address_Tx[4];
+    unsigned char NwkSKey[16];
+    unsigned char AppSKey[16];
+    unsigned int Frame_Counter_Tx;
+    sLoRa_Session Session_Data;
+
+    // Declare OTAA data struct
+    unsigned char DevEUI[8];
+    unsigned char AppEUI[8];
+    unsigned char AppKey[16];
+    unsigned char DevNonce[2];
+    unsigned char AppNonce[3];
+    unsigned char NetID[3];
+    sLoRa_OTAA OTAA_Data;
+
+    // Declare LoRA settings struct
+    sSettings LoRa_Settings;
+    sRFM_pins LoRa_Pins;
+
+    unsigned char drate_common;
+
+    // Lora Setting Class
+    devclass_t dev_class;
+
+    // channel mode
+    unsigned char currentChannel;
+
+    // UART
+    RFM_command_t RFM_Command_Status;
+    rx_t Rx_Status;
+
+    // ACK reception
+    ack_t Ack_Status;
+};
+
+#endif
