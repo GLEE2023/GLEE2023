4c95fee62449b7c4062aff6fd3c4d56f08fc4da3
MattEMoran <mamo2629@colorado.edu>
2022-04-25 17:30:02 -0600

    Update Magnetometer_New_Vector_Plot.ino

--- examples/GLEE_MLX90393_Examples/Magnetometer_New_Vector_Plot/Magnetometer_New_Vector_Plot.ino
+++ examples/GLEE_MLX90393_Examples/Magnetometer_New_Vector_Plot/Magnetometer_New_Vector_Plot.ino
@@ -1,17 +1,14 @@
-#include "MLX90395.h"
+#include “MLX90395.h”
 
 MLX90395 magnetometer = MLX90395(1,false); // Set-up for the magnetometer with debugging set to false
 
 mlx_sample_t sample; // Saves the sample
-
 float angle; // Saves the measured angle
 float magnitude; // Saves the measured magnitude
-
 float avgX = 0.0; // Saves average axes measurements
 float avgY = 0.0;
 float avgZ = 0.0;
-
-String input = ""; // Variable reserved for user input
+String input = “”; // Variable reserved for user input
 
 void setup (){
     Serial.begin(9600);
@@ -19,33 +16,29 @@ void setup (){
     magnetometer.begin_I2C();
 
     // Set gain
-    magnetometer.setGain(gainMultipliers[0]);
+    //magnetometer.setGain(gainMultipliers[0]);
 
     // Set resolution
-    magnetometer.setResolution(MLX90393_RES_19);
-    //magnetometer.setResolution(MLX90393_RES_19);
-    //magnetometer.setResolution(MLX90393_RES_16);
+    magnetometer.setResolution(MLX90395_RES_19,MLX90395_RES_19,MLX90395_RES_16);
 
     // Set oversampling
-    magnetometer.setOversampling(MLX90393_OSR_2);
+    magnetometer.setOSR(MLX90395_OSR_2);
 
     // Set digital filtering
-    //magnetometer.setFilter(MLX90393_FILTER_6);
+    magnetometer.setFilter(MLX90395_FILTER_6);
 
     // Calibration
-    Serial.println("Place your LunaSat at your desk with the magnet away.");
-    Serial.println("Your LunaSat should be oriented such that the solar panels are at the top.");
-    Serial.println("Press Enter to calibrate your magnetometer.");
-
+    Serial.println(“Place your LunaSat at your desk with the magnet away.“);
+    Serial.println(“Your LunaSat should be oriented such that the solar panels are at the top.“);
+    Serial.println(“Press Enter to calibrate your magnetometer.“);
     while(!Serial.available()){} // While no input from user, wait
     input = Serial.readString();
 
     // Get calibration points
-    mlx_sample_t calibrationPoints[20];    
+    mlx_sample_t calibrationPoints[20];
     for(int i = 0; i < 20; i++){
       calibrationPoints[i] = magnetometer.getSample();
     }
-
     float avgX = 0.0;
     float avgY = 0.0;
     float avgZ = 0.0;
@@ -61,15 +54,13 @@ void setup (){
     avgX = avgX/20;
     avgY = avgY/20;
     avgZ = avgZ/20;
-
-    Serial.println("Calibration is complete. You may now begin the activity.");
+    Serial.println(“Calibration is complete. You may now begin the activity.“);
     Serial.println();
 }
-
 void loop (){
-  Serial.println("Place your LunaSat above the point where you wish to draw a vector.");
-  Serial.println("Press Enter to generate a vector.");
-  
+  Serial.println(“Place your LunaSat above the point where you wish to draw a vector.“);
+  Serial.println(“Press Enter to generate a vector.“);
+
   // Wait for continue command from user (currently accepts any input)
   while(!Serial.available()){}
   input = Serial.readString();
@@ -88,25 +79,25 @@ void loop (){
   } else {
       angle = 0.0;
   }
+  String direction_str = “”;
 
-  String direction_str = "";
   // Determine the direction of magnetic field vector based on the angle
   if (angle <= 22.5 || angle > 337.5){
-      direction_str = "Up";
+      direction_str = “Up”;
   } else if (angle <= 67.5 && angle > 22.5){
-      direction_str = "Up Left";
-  } else if (angle <= 112.5 && angle > 67.5){   
-      direction_str = "Left";
-  } else if (angle <= 157.5 && angle > 112.5){  
-      direction_str = "Down Left";
-  } else if (angle <= 202.5 && angle > 157.5){    
-      direction_str = "Down";
-  } else if (angle <= 247.5 && angle > 202.5){   
-      direction_str = "Down Right";
-  } else if (angle <= 292.5 && angle > 247.5){    
-      direction_str = "Right";
-  } else if (angle <= 337.5 && angle > 292.5){   
-      direction_str = "Up Right";
+      direction_str = “Up Left”;
+  } else if (angle <= 112.5 && angle > 67.5){
+      direction_str = “Left”;
+  } else if (angle <= 157.5 && angle > 112.5){
+      direction_str = “Down Left”;
+  } else if (angle <= 202.5 && angle > 157.5){
+      direction_str = “Down”;
+  } else if (angle <= 247.5 && angle > 202.5){
+      direction_str = “Down Right”;
+  } else if (angle <= 292.5 && angle > 247.5){
+      direction_str = “Right”;
+  } else if (angle <= 337.5 && angle > 292.5){
+      direction_str = “Up Right”;
   }
 
   // Calculates magnitude of vector from x and y axes measurements only
@@ -114,14 +105,13 @@ void loop (){
 
   // Prints direction and magnitude of vector at current location.
   Serial.println();
-  Serial.print(String("Vector Data - "));
-  Serial.print(String("Direction: " + direction_str + " , ")); 
+  Serial.print(String(“Vector Data - “));
+  Serial.print(String(“Direction: ” + direction_str + ” , “));
   
   // Length calculations
-  // A vector of magnitude 1000.0 uT translates to a 2cm vector, 
+  // A vector of magnitude 1000.0 uT translates to a 2cm vector,
   // but this is not a strictly defined limit
-  magnitude = ((2) / (1000.0)) * (magnitude); 
-  
-  Serial.println(String("Magnitude: " + String(magnitude) + " cm"));
+  magnitude = ((2) / (1000.0)) * (magnitude);
+  Serial.println(String(“Magnitude: ” + String(magnitude) + ” cm”));
   Serial.println();
 }
