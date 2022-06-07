6ae5bf2e36f66f6dbb5408ad92f3322fe9fb2dad
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2022-06-07 12:36:24 -0600

    Updated to print data cleaner

--- examples/GLEE_MLX90393_Examples/Magnetometer_New_Basic_Example/Magnetometer_New_Basic_Example.ino
+++ examples/GLEE_MLX90393_Examples/Magnetometer_New_Basic_Example/Magnetometer_New_Basic_Example.ino
@@ -1,50 +1,45 @@
-
 #include "MLX90395.h"
 
-MLX90395 magnetometer = MLX90395(1,false);
+MLX90395 magnetometer = MLX90395(1,false); // Set-up for the magnetometer with debugging set to false
 
-mlx_sample_t sample;
-float strength;
+
 
 void setup (){
     Serial.begin(9600);
 
-    if (! magnetometer.begin_I2C()) {          // hardware I2C mode, can pass in address & alt Wire
+    if (!magnetometer.begin_I2C()) {          // hardware I2C mode, can pass in address & alt Wire
         Serial.println("No sensor found ... check your wiring?");
         while (1) { delay(10); }
     }
 
-    //magnetometer.begin_I2C();
-
     // Set gain
-    //magnetometer.setGain(gainMultipliers[16]);
+    //magnetometer.setGain(MLX90393_GAIN_2_5X); //this line may or may not work
 
     // Set resolution
-    magnetometer.setResolution(MLX90395_RES_19, MLX90395_RES_19, MLX90395_RES_16);
+    magnetometer.setResolution(MLX90395_RES_19,MLX90395_RES_19,MLX90395_RES_16);
+
     // Set oversampling
     magnetometer.setOSR(MLX90395_OSR_2);
 
     // Set digital filtering
     magnetometer.setFilter(MLX90395_FILTER_6);
 
+
+    //Setup done
+    Serial.println("Begin printing data:");
+    Serial.println("X [uT],Y [uT],Z [uT],Magnitude [uT]");
 };
 
-void loop (){   
-    
-    sample = magnetometer.getSample();
-    strength = sqrt(pow(sample.magnetic.x,2) + pow(sample.magnetic.y,2) + pow(sample.magnetic.z,2));
-    // Print out magnetic field measurements for each axis 
-    Serial.println("Magnetic Field Axes Measurements");
-    Serial.print("X: "); Serial.print(sample.magnetic.x,4);
-    Serial.print("\tY: "); Serial.print(sample.magnetic.y,4);
-    Serial.print("\tZ: "); Serial.print(sample.magnetic.z,4);
-    Serial.println(" uT");
-    // Print out strength of magnetic field
-    Serial.println("Magnetic Field Strength (Magnitude)"); 
-    Serial.print(strength,4); Serial.println(" uT");
-    Serial.println();
-   
-    delay(500); // Take samples every one second
-    
+void loop (){
+    /* Get a new sensor event, normalized to uTesla */
+    mlx_sample_t sample = magnetometer.getSample();
 
+    // get strength by square rooting the sum of squares of each element
+    float strength = sqrt(pow(sample.magnetic.x,2) + pow(sample.magnetic.y,2) + pow(sample.magnetic.z,2));
+
+    // Print out magnetic field measurements for each axis
+    Serial.print(sample.magnetic.x,4); Serial.print(","); Serial.print(sample.magnetic.y,4); Serial.print(",");
+    Serial.print(sample.magnetic.z,4); Serial.print(","); Serial.print(strength,4); Serial.println("");
+
+    delay(1000); // Take samples approx every one second
 };
