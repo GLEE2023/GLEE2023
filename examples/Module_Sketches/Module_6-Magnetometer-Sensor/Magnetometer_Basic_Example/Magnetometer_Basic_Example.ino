4f9516f172c5c46302d1dae0505dbb2d298ebebc
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2022-06-15 16:17:47 -0600

    Modified to match what is on the module 6 slide

--- examples/Module_Sketches/Module_6-Magnetometer-Sensor/Magnetometer_Basic_Example/Magnetometer_Basic_Example.ino
+++ examples/Module_Sketches/Module_6-Magnetometer-Sensor/Magnetometer_Basic_Example/Magnetometer_Basic_Example.ino
@@ -2,11 +2,11 @@
 
 MLX90395 magnetometer = MLX90395(1,false); // Set-up for the magnetometer with debugging set to false
 
+mlx_sample_t sample;
 
 
 void setup (){
     Serial.begin(9600);
-
     if (!magnetometer.begin_I2C()) {          // hardware I2C mode, can pass in address & alt Wire
         Serial.println("No sensor found ... check your wiring?");
         while (1) { delay(10); }
@@ -28,7 +28,7 @@ void setup (){
     // Set oversampling. See 18.4 in the datasheet
     magnetometer.setOSR(MLX90395_OSR_4); //not default. default is MLX90395_OSR_1 and is equivalent to 0
 
-    // Set digital filtering. (Built in rolling average)
+    // Set digital filtering. (Built in averaging)
     magnetometer.setFilter(MLX90395_FILTER_8); //default. MLX90395_FILTER_1 is equivalent to 0
 
 
@@ -39,16 +39,21 @@ void setup (){
 };
 
 void loop (){
-    /* Get a new sensor event, normalized to uTesla */
-    mlx_sample_t sample = magnetometer.getSample();
+    // Get a sample
+    sample = magnetometer.getSample();
 
     // get strength by square rooting the sum of squares of each element
     float strength = sqrt(pow(sample.magnetic.x,2) + pow(sample.magnetic.y,2) + pow(sample.magnetic.z,2));
+
+    //get angle using atan2(). Returns value from -pi to pi
     float angle = atan2(sample.magnetic.y,sample.magnetic.x)*180/3.14;
 
     // Print out magnetic field measurements for each axis
-    Serial.print(sample.magnetic.x,4); Serial.print(", \t"); Serial.print(sample.magnetic.y,4); Serial.print(", \t");
-    Serial.print(sample.magnetic.z,4); Serial.print(", \t"); Serial.print(strength,4); Serial.print(", \t"); Serial.print(angle,4); Serial.println("");
+    Serial.print(sample.magnetic.x,4); Serial.print(", \t");
+    Serial.print(sample.magnetic.y,4); Serial.print(", \t");
+    Serial.print(sample.magnetic.z,4); Serial.print(", \t");
+    Serial.print(strength,4); Serial.print(", \t");
+    Serial.print(angle,4); Serial.println("");
 
     delay(100); // Take samples approx 10Hz
 };
