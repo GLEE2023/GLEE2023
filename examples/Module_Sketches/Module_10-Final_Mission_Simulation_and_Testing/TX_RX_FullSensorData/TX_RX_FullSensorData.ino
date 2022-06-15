7c4f394a60495baffe7e35e57fb48f764d787383
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2022-06-15 15:16:25 -0600

    Modified to add MLX'95 functionality to TX_RX in Module 10

--- examples/Module_Sketches/Module_10-Final_Mission_Simulation_and_Testing/TX_RX_FullSensorData/TX_RX_FullSensorData.ino
+++ examples/Module_Sketches/Module_10-Final_Mission_Simulation_and_Testing/TX_RX_FullSensorData/TX_RX_FullSensorData.ino
@@ -1,6 +1,6 @@
 // Sensor Includes
 #include "TMP117.h"
-#include "MLX90393.h"
+#include "MLX90395.h"
 #include "MPU6000.h"
 #include "CAP.h"
 #include "TPIS1385.h"
@@ -12,7 +12,7 @@ LunaRadio Rad;
 // Sensor Objects (Debugging defaults to false)
 TMP117 thermometer(1);
 MPU6000 accelerometer(2);
-MLX90393 magnetometer(3);
+MLX90395 magnetometer(3);
 TPIS1385 thermopile(4);
 CAP capacitive(5);
 
@@ -37,7 +37,7 @@ char thermAmb_str[6] = {' '} ;
 char cap_str[5] = {' '};
 
 // Transmission Buffer
-char data_buffer[80]; 
+char data_buffer[80];
 
 // Output Formatting Variables
 String printOut;
@@ -53,22 +53,21 @@ void setup() {
 
     // SENSOR INITIALIZATIONS
 
-    // Temperature Sensor - TMP117 
+    // Temperature Sensor - TMP117
     // (Nothing)
-    
+
     // Accelerometer - MPU6000
     accelerometer.begin(); // Begin (check connection)
     accelerometer.initialize(); // Initialize (setup)
     accelerometer.setAccelRange(MPU6000_RANGE_2_G); // Make sure acceleration range is set to 2G
 
-    // Magnetometer - MLX90393    
-    magnetometer.begin_I2C(); // Begin
-    magnetometer.setGain(MLX90393_GAIN_2_5X); // Set gain (signal amplification)
-    magnetometer.setResolution(MLX90393_X, MLX90393_RES_19); // Set resolutions on all 3 axis
-    magnetometer.setResolution(MLX90393_Y, MLX90393_RES_19);
-    magnetometer.setResolution(MLX90393_Z, MLX90393_RES_16);
-    magnetometer.setOversampling(MLX90393_OSR_2); // Set oversampling
-    magnetometer.setFilter(MLX90393_FILTER_6); // Set digital filtering
+    // Magnetometer - MLX90393
+    magnetometer.begin_I2C();
+    // Set gain, resolution,OSR, and digital filter
+    magnetometer.setGain(8); //default value is 8, can range from 0-15
+    magnetometer.setResolution(MLX90395_RES_17,MLX90395_RES_17,MLX90395_RES_17);
+    magnetometer.setOSR(MLX90395_OSR_4); //not default. default is MLX90395_OSR_1 and is equivalent to 0
+    magnetometer.setFilter(MLX90395_FILTER_8); //default. MLX90395_FILTER_1 is equivalent to 0
 
     // Capacitive Sensor CAP
     capacitive.begin(); // Begin
@@ -97,11 +96,11 @@ void loop() {
 
     // Decimal TO STRing Functions to load decimal (float) samples as strings into individual buffers
     // see https://www.programmingelectronics.com/dtostrf/ for more info
-    dtostrf(tempSample,3,3,temp_str); 
+    dtostrf(tempSample,3,3,temp_str);
 
     dtostrf(accSample.x,1,4,accX_str);
-    dtostrf(accSample.y,1,4,accY_str); 
-    dtostrf(accSample.z,1,4,accZ_str); 
+    dtostrf(accSample.y,1,4,accY_str);
+    dtostrf(accSample.z,1,4,accZ_str);
 
     dtostrf(magSample.magnetic.x,2,2,magX_str);
     dtostrf(magSample.magnetic.y,2,2,magY_str);
@@ -115,38 +114,38 @@ void loop() {
     sprintf(cap_str, "%i", capSample);
 
     // Using STRing CoPY to construction of transmission buffer with ID for transmission filtering
-    
+
     ///////////////////////////////////////////////
     strcpy(data_buffer, "11,"); // CHANGE TO YOUR TEAM NUMBER
     ///////////////////////////////////////////////
 
-    // Using STRing ConCaT to append indiviual character arrays (psudo strings) to 
+    // Using STRing ConCaT to append indiviual character arrays (psudo strings) to
     strcat(data_buffer, timeStamp_str);
     strcat(data_buffer, ",");
     strcat(data_buffer, temp_str);
     strcat(data_buffer, ",");
-    strcat(data_buffer, accX_str); 
+    strcat(data_buffer, accX_str);
     strcat(data_buffer, ",");
-    strcat(data_buffer, accY_str); 
+    strcat(data_buffer, accY_str);
     strcat(data_buffer, ",");
-    strcat(data_buffer, accZ_str); 
+    strcat(data_buffer, accZ_str);
     strcat(data_buffer, ",");
-    strcat(data_buffer, magX_str); 
+    strcat(data_buffer, magX_str);
     strcat(data_buffer, ",");
-    strcat(data_buffer, magY_str); 
+    strcat(data_buffer, magY_str);
     strcat(data_buffer, ",");
-    strcat(data_buffer, magZ_str); 
+    strcat(data_buffer, magZ_str);
     strcat(data_buffer, ",");
-    strcat(data_buffer, thermObj_str); 
+    strcat(data_buffer, thermObj_str);
     strcat(data_buffer, ",");
-    strcat(data_buffer, thermAmb_str); 
+    strcat(data_buffer, thermAmb_str);
     strcat(data_buffer, ",");
-    strcat(data_buffer, cap_str); 
+    strcat(data_buffer, cap_str);
 
     // Print data (already in string format) to serial monitor(wired)
     Serial.println(data_buffer);
 
-    // Transmit string data (already in string format) to 
+    // Transmit string data (already in string format) to
     Rad.transmit_data(data_buffer);
 
     // Note: Optimized transmission of data is in progress... String formatting for demo purposes
@@ -155,7 +154,7 @@ void loop() {
     digitalWrite(LED, HIGH);
     delay(200);
     digitalWrite(LED, LOW);
-  
+
      // Repeate every ~500ms INCREASE TO CHANGE SAMPLE RATE
     delay(500);
 }
