d703f11a29d4d89e2f3c8542107121c2da244902
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2022-07-27 13:18:36 -0600

    added cpp for Lunar MPU. Moved Wire funcitons to their own file

--- examples/Lunar_Sketches/LunaSat_Mode_Config/LunaSat_Mode_Config.ino
+++ examples/Lunar_Sketches/LunaSat_Mode_Config/LunaSat_Mode_Config.ino
@@ -32,8 +32,6 @@ INSERT_NUM = averaging bits, see convTimes table.
 #define MODE_2_DURATION 200
 
 
-void writeByte (uint8_t registerAddress, uint8_t writeData);
-void readBytes (uint8_t registerAddress, uint8_t nBytes, uint8_t * data);
 
 void setup(){
   //setup
@@ -70,28 +68,3 @@ void setConfig(long config){
   // setCAP()
   // setBM14()
 }
-
-// void readBytes(uint8_t I2CsensorAddress, uint8_t registerAddress, uint8_t nBytes, uint8_t * data){
-//     Wire.beginTransmission(I2CsensorAddress);           // begins forming transmission to sensor
-//     Wire.write(registerAddress);                     // Add register address to transmission
-//     Wire.endTransmission();
-//     Wire.requestFrom(I2CsensorAddress, nBytes);         // Request and listen for response
-//     // Record response, wire will be available until nBytes are read
-//     int i = 0;
-//     while(Wire.available()){
-//         data[i] = Wire.read();
-//         i++;
-//     }
-// }
-//
-// /**
-//  * Parameters: Register Address, Write Data
-//  * Returns: None
-//  * This function writes data to specified address
-// **/
-// void writeByte (uint8_t I2CsensorAddress, uint8_t registerAddress, uint8_t writeData){
-//     Wire.beginTransmission(I2CsensorAddress);               // begin communication with the sensor
-//     Wire.write(registerAddress);                                // point to address to be written to
-//     Wire.write(writeData);                                      // write data to adress specificed above
-//     Wire.endTransmission();                                     // end communication
-// }
--- src/Lunar_I2C.h
+++ src/Lunar_I2C.h
@@ -1,6 +1,13 @@
 #include <Wire.h>
 
-void readBytes(uint8_t I2CsensorAddress, uint8_t registerAddress, uint8_t nBytes, uint8_t * data){
+//this utilizes an anonamous namespace... Not exactly sure why this is needed, but the compiler gets mad if it isn't used
+namespace {
+  /**
+  * Parameters: I2C address, Register Address, Bytes to read, buffer to read to
+  * Returns: None
+  * This function reads data from a specified I2c address, and specific register
+  **/
+  void readBytes(uint8_t I2CsensorAddress, uint8_t registerAddress, uint8_t nBytes, uint8_t * data){
     Wire.beginTransmission(I2CsensorAddress);           // begins forming transmission to sensor
     Wire.write(registerAddress);                     // Add register address to transmission
     Wire.endTransmission();
@@ -8,19 +15,20 @@ void readBytes(uint8_t I2CsensorAddress, uint8_t registerAddress, uint8_t nBytes
     // Record response, wire will be available until nBytes are read
     int i = 0;
     while(Wire.available()){
-        data[i] = Wire.read();
-        i++;
+      data[i] = Wire.read();
+      i++;
     }
-}
+  }
 
-/**
- * Parameters: Register Address, Write Data
- * Returns: None
- * This function writes data to specified address
-**/
-void writeByte (uint8_t I2CsensorAddress, uint8_t registerAddress, uint8_t writeData){
+  /**
+  * Parameters: Register Address, Write Data
+  * Returns: None
+  * This function writes data to specified address
+  **/
+  void writeByte (uint8_t I2CsensorAddress, uint8_t registerAddress, uint8_t writeData){
     Wire.beginTransmission(I2CsensorAddress);               // begin communication with the sensor
     Wire.write(registerAddress);                                // point to address to be written to
     Wire.write(writeData);                                      // write data to adress specificed above
     Wire.endTransmission();                                     // end communication
+  }
 }
