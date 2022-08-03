cea5414cd097413a0d3b245d818d433304d36c9d
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2022-08-03 09:18:32 -0600

    Made lunar scripts for BM14. Not working

--- examples/Lunar_Sketches/LunaSat_Mode_Config/LunaSat_Mode_Config.ino
+++ examples/Lunar_Sketches/LunaSat_Mode_Config/LunaSat_Mode_Config.ino
@@ -1,34 +1,36 @@
 /*
 From LSB to MSB:
 -TMP117 (20-25)
-24-25 = mode
-21-23 = conversion bits, see convTimes table.
-20-21 = averaging bits, see convTimes table.
+25-26 = mode
+22-24 = conversion bits, see convTimes table.
+21-22 = averaging bits, see convTimes table.
 
 -MPU6000 (5-19)
-17-19 = digital low pass setting, may or may not be removed.
-9-16 = sample rate divisor, number between 0-255
-5-8 = mode, options are in generateBitstrings file.
+18-20 = digital low pass setting, may or may not be removed.
+8-17 = sample rate divisor, number between 0-255
+6-9 = mode, options are in generateBitstrings file.
 
 -Therm (4)
-4 = on or off (doesn't do anything)
+5 = on or off (doesn't do anything)
 
 -Cap (3)
-3 = on or off (doesn't actually do anything)
+4 = on or off (doesn't actually do anything)
 
 -Mag (0-2)
-0 = on or off, 0 is off
-1-2 = mode, represents frequency, one more bit needed.
+3   = on or off, 0 is off
+2   = cc or single shot
+0-1 = cc freq
 */
 
 #include <Arduino.h>
 #include <Wire.h>
 #include "MPU6000_Lunar.h"
 #include "TMP117_Lunar.h"
+#include "BM1422AGMV_Lunar.h"
 
 // #define DBUG true
 
-#define MODE_1_CONFIG 0b01000000000000000001011100
+#define MODE_1_CONFIG 0b010000000000000000010111000
 
 #define MODE_2_CONFIG 10952
 
@@ -53,15 +55,24 @@ void loop(){
   uint8_t buffer[6];
 
   //get data from accelerometer
-  MPU6000_Lunar::readAccData(buffer);
-  float acc_z = (float)(buffer[4] << 8 |buffer[5]) / 16340.0 * -1;
+  // MPU6000_Lunar::readAccData(buffer);
+  // float acc_z = (float)(buffer[4] << 8 | buffer[5]) / 16340.0 * -1;
 
   //get data from temperature sensor
-  TMP117_Lunar::getData(buffer);
-  float temp = TMP117_Lunar::convertToTempC(buffer);
+  // TMP117_Lunar::getData(buffer);
+  // float temp = TMP117_Lunar::convertToTempC(buffer);
 
-  Serial.print(temp); Serial.print(", ");
-  Serial.println(acc_z);
+  Serial.println("Mag stuff:");
+  BM1422AGMV_Lunar::getData(buffer);
+  unsigned short val = (signed short)(buffer[1]<<8) | (buffer[0]);
+  Serial.print("VAL: "); Serial.println(val);
+  float mag_x = (float)val / 6;
+
+  // Serial.print(temp); 
+  // Serial.print(", ");
+  Serial.print(mag_x);
+  // Serial.print(", ");
+  // Serial.println(acc_z);
 }
 
 void setConfig(long config){
@@ -69,8 +80,9 @@ void setConfig(long config){
   Serial.println(config,HEX);
 
   // setTMP((config<<0) & 0xFF);
-  MPU6000_Lunar::setConfig((config>>4) & 0x7FF);
-  TMP117_Lunar::setConfig((config>>19) & 0x3F);
+  MPU6000_Lunar::setConfig((config>>5) & 0x7FF);
+  TMP117_Lunar::setConfig((config>>20) & 0x3F);
+  BM1422AGMV_Lunar::setConfig(config & 0xF);
   // setTPIS((config<<23) & 0x1);
   // setCAP()
   // setBM14()
---
+++ src/BM1422AGMV_Lunar.cpp
@@ -0,0 +1,41 @@
+#include "BM1422AGMV_Lunar.h"
+
+/*
+-Mag (0-2)
+3   = on or off, 0 is off
+2   = cc or single shot
+0-1 = cc freq
+*/
+
+void BM1422AGMV_Lunar::setConfig(uint8_t config){
+  /*
+  contr1 register
+  7 - power
+  6 - out bit (0: 12 bit, 1: 14 bit )
+  5 - rst: logic reset control
+  3-4 ODR: measurment output rate (00:10Hz, 10: 20Hz, 01:100Hz, 11:1KHz) odd numbering, but it is correct
+  1 - FS1: 0: Continuous, 1: single mode
+  */
+
+  #ifdef BM1422AGMV_DEBUG
+    Serial.print(F("BM14 CONFIG STR: 0x")); Serial.print(config,HEX); Serial.print(F(", CONTR1 val (hex):")); Serial.println(((config & 0x8)<<4) | ((config & 0x3)<<3) | ((config & 0x01)<<0),HEX);
+  #endif
+
+  //see page 16 in the BM1422AGMV data sheet for steps to set mode
+  //step 1
+  Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL1,((config & 0x8)<<7) | (0x40) | ((config & 0x3)<<3) | ((config & 0x01)<<0));
+  Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL4,0x00);
+  Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL4+1,0x00);
+  //step2
+  Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL2,0x0C);
+  //step 3 is to write offset values, not sure this is necessary
+
+  //step4
+  Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL3,BM1422AGMV_CNTL3_FORCE); //starts measure
+
+}
+
+void BM1422AGMV_Lunar::getData(uint8_t* buffer){
+  Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL3+1, 0x4);
+  Lunar_I2C::readBytes(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_DATAX, 6, buffer);
+}
