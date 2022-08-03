7606900ce652e0da9935c2bd694d78a7756006e5
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2022-08-03 09:51:36 -0600

    fixed bit shift bug

--- src/BM1422AGMV_Lunar.cpp
+++ src/BM1422AGMV_Lunar.cpp
@@ -23,7 +23,7 @@ void BM1422AGMV_Lunar::setConfig(uint8_t config){
 
   //see page 16 in the BM1422AGMV data sheet for steps to set mode
   //step 1
-  Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL1,((config & 0x8)<<7) | (0x40) | ((config & 0x3)<<3) | ((config & 0x01)<<0));
+  Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL1,((config & 0x8)<<4) | (0x40) | ((config & 0x3)<<3) | ((config & 0x01)<<0));
   Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL4,0x00);
   Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL4+1,0x00);
   //step2
@@ -36,6 +36,7 @@ void BM1422AGMV_Lunar::setConfig(uint8_t config){
 }
 
 void BM1422AGMV_Lunar::getData(uint8_t* buffer){
-  Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL3+1, 0x4);
+  Lunar_I2C::writeByte(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_CNTL3, 0x04);
+  delay(10);
   Lunar_I2C::readBytes(BM1422AGMV_DEVICE_ADDRESS_0E,BM1422AGMV_DATAX, 6, buffer);
 }
